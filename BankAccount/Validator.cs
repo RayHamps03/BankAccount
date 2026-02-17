2e1a8cee495536c748e8e6b9c167fba416426da0
Raymond Hampson III <rayhampson4@gmail.com>
2026-02-16 21:03:45 -0800

    Add project files.

---
+++ BankAccount/Validator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Validator
+    {
+        public bool IsWithinRange(double value, double min, double max)
+        {
+            if (value <= min)
+            {
+                return true;
+            }
+            if (value > max)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
